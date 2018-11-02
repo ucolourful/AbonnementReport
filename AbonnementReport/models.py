b0d2abfc3a8d5bfeb28c2bc04ab1eb7473605289
ucolourful <506231730@qq.com>
2018-11-02 17:29:02 +0800

    Update models.py
    [提交原因]: 首页完成产品线、注销、用户权限
    [提交时间]: 2018年11月2日17:25:53

--- AbonnementReport/models.py
+++ AbonnementReport/models.py
@@ -4,7 +4,18 @@ from django.db import models
 
 
 class UserClass(models.Model):
+    """
+    用户数据表
+    """
     userName = models.CharField(max_length=30)
     passWord = models.CharField(max_length=30)
     emailAddr = models.CharField(max_length=30)
     productLine = models.CharField(max_length=30)
+
+
+class UserAuth(models.Model):
+    """
+    用户权限表
+    """
+    userName = models.CharField(max_length=30)
+    userAuth = models.CharField(max_length=30)
