b30c1b28a95d0cb05dda3d5deb039b847ed66cfc
ucolourful <506231730@qq.com>
2018-11-05 20:29:57 +0800

    Update ARIndex.js
    [提交原因]: 首页功能完成
    [提交时间]: 2018年11月5日20:28:01

--- static/javascript/ARIndex.js
+++ static/javascript/ARIndex.js
@@ -7,9 +7,11 @@ function addVersion(){
             alert("请输入版本号进行添加！！！")
             return
         }
-        $.post("/addVersion",{'versionName':versionName},function(ret){
-            location.reload()
-        })
+        if ( confirm("请确认是否添加此版本？") == true){
+            $.post("/addVersion",{'versionName':versionName},function(ret){
+                location.reload()
+            })
+        }
     }
     else {
         alert("您没有添加版本的权限！！！");
@@ -22,12 +24,62 @@ function delVersion(event){
     userAuth = document.getElementById("userAuth").value;
     if ( userAuth == "admin" ) {
         var versionID = event.name.split("_")[1]
-        $.post("/delVersion",{'versionID':versionID},function(ret){
-            location.reload()
-        })
+        if ( confirm("请确认是否删除此版本？") == true){
+            $.post("/delVersion",{'versionID':versionID},function(ret){
+                location.reload()
+            })
+        }
     }
     else {
         alert("您没有删除版本的权限！！！");
         return
     }
+}
+
+// 订阅版本
+function abonnementReport(event){
+    // 获取versionID
+    versionID = event.name.split("_")[1]
+
+    // 已订阅,这里处理"取消订阅"的功能
+    if (event.value == "0"){
+        if ( confirm("请确认是否取消订阅此版本邮件？") == true){
+            $.post("/abonnementReport",{'versionID':versionID,'doAction':event.value},function(ret){
+                location.reload()
+            })
+            return
+        }
+        return
+    }
+    // 未订阅,这里处理"订阅报告"的功能
+
+    if (event.value == "1"){
+        if ( confirm("请确认是否订阅此版本邮件？") == true){
+            $.post("/abonnementReport",{'versionID':versionID,'doAction':event.value},function(ret){
+                location.reload()
+            })
+            return
+        }
+        return
+    }
+}
+
+// 查看订阅人
+function viewVersionUsers(event){
+    // 获取版本名称
+    versionName = event.name
+
+    // 查看单个版本订阅人
+    if ( versionName != "viewAllAbUser" ){
+        $.get("/viewVersionUsers",{'versionName':versionName},function(ret){
+            var obj = JSON.parse(JSON.stringify(ret))
+            alert(obj.userList)
+        },"json")
+    }
+    else{
+        $.get("/viewVersionUsers",function(ret){
+            alert(JSON.stringify(ret))
+        },"json")
+    }
+    return
 }
