b0a1d629eaa41336b8091647523528b15569fc27
ucolourful <506231730@qq.com>
2018-11-09 20:42:27 +0800

    Create ARlogin.js
    [提交原因]: 
    重构，使用layui框架
    [提交时间]: 2018年11月9日20:40:23

---
+++ static/javascript/ARlogin.js
@@ -0,0 +1,30 @@
+//AR 注册页 JS
+layui.use(['element','layer','form'], function () {
+    var element = layui.element;
+    var form = layui.form;
+
+    //自定义验证规则
+    form.verify({
+        title: function (value) {
+            if (value.length < 5) {
+                return '用户名也太短了吧';
+            }
+        }
+        , pass: [/(.+){6,12}$/, '密码必须6到12位']
+    });
+
+    //监听提交
+    form.on('submit(*)', function (data) {
+        $.post("/userLogin",data.field,function(ret){
+            if ( ret.status == 0 ){
+                layer.msg(ret.msg)
+                setTimeout("window.location.href='/index'",2000)
+            }
+            if ( ret.status == 1 ){
+                layer.msg(ret.msg)
+            }
+        })
+//        layer.msg(JSON.stringify(data.field))
+        return false
+    });
+});
