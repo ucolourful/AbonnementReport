931f9d0a3c61bbe4cae6c5d3c604f90803f1ecb2
ucolourful <506231730@qq.com>
2018-11-09 20:42:04 +0800

    Update ARregist.js
    [提交原因]: 
    重构，使用layui框架
    [提交时间]: 2018年11月9日20:40:23

--- static/javascript/ARregist.js
+++ static/javascript/ARregist.js
@@ -1,5 +1,5 @@
 //AR 注册页 JS
-layui.use(['element', 'form'], function () {
+layui.use(['element','layer','form'], function () {
     var element = layui.element;
     var form = layui.form;
 
@@ -7,28 +7,30 @@ layui.use(['element', 'form'], function () {
     form.verify({
         title: function (value) {
             if (value.length < 5) {
-                return '标题也太短了吧';
+                return '用户名也太短了吧';
             }
         }
         , pass: [/(.+){6,12}$/, '密码必须6到12位']
+        , pass2: function(value) {
+            if ( value != $("#password").val() ) {
+                $("#password2").val("")
+                return '两次输出的密码不一致吧'
+            }
+        }
     });
 
-    //初始赋值
-    // form.val('first', {
-    //     'username': '测试11111'
-    //     , 'password': 123123
-    //     , 'password2': 123123
-    //     , 'email': 'xu@sentsin.com'
-    //     , 'productline': '虚拟存储VS'
-    // });
-
-    form.on('select(interest)', function (data) {
-        console.log('select.interest: ', this, data);
-    });
     //监听提交
     form.on('submit(*)', function (data) {
-        console.log(data);
-        alert(JSON.stringify(data.field));
-        // return false;
+        $.post("/userRegist",data.field,function(ret){
+            if ( ret.status == 0 ){
+                layer.msg(ret.msg)
+                setTimeout("window.location.href='/index'",2000)
+            }
+            if ( ret.status == 1 ){
+                layer.msg(ret.msg)
+            }
+        })
+//        layer.msg(JSON.stringify(data.field))
+        return false
     });
 });
