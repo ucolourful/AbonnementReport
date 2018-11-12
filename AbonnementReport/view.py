124e4e6e1ffefea25436a9a516edde32bfb518ef
ucolourful <506231730@qq.com>
2018-11-12 19:22:16 +0800

    Update view.py
    [提交原因]: 
    重构，使用layui框架。切换完毕。
    下一步精简资源
    [提交时间]: 2018年11月12日19:15:34

--- AbonnementReport/view.py
+++ AbonnementReport/view.py
@@ -158,15 +158,15 @@ def abonnementReport(request):
     if "username" not in request.session or "userAuth" not in request.session or "productLine" not in request.session:
         return redirect('/login')
 
-    # 未传入versionID，返回首页
+    # 未传入versionID
     if "versionID" not in request.POST or "doAction" not in request.POST:
-        return redirect('/index')
+        return HttpResponse(json.dumps({"status": 1, "msg": "操作失败！请重新登录进行尝试！"}), content_type="application/json")
 
     # 判断，是否存在versionID的版本、用户是否存在，任意不存在返回首页
     versionIDList = ProductVersion.objects.filter(id=int(request.POST["versionID"]))
     userList = UserClass.objects.filter(userName=request.session["username"])
     if len(versionIDList) == 0 or len(userList) == 0:
-        return redirect('/index')
+        return HttpResponse(json.dumps({"status": 1, "msg": "操作失败！请重新登录进行尝试！"}), content_type="application/json")
 
     # 获取用户、版本和订阅表信息
     proVersion = ProductVersion.objects.get(id=int(request.POST["versionID"]))
@@ -179,13 +179,15 @@ def abonnementReport(request):
         # 保存订阅信息到订阅数据表
         ac = AbonnementClass(versionName=proVersion.versionName, userName=user.userName, emailAddr=user.emailAddr)
         ac.save()
+        return HttpResponse(json.dumps({"status": 0, "msg": "订阅成功！"}), content_type="application/json")
 
     # "doAction==0"用户进行取消订阅操作；并判断用户是否订阅过此版本，未订阅则返回到首页
     elif "0" == request.POST["doAction"] and len(acList) == 1:
         ac = AbonnementClass.objects.get(versionName=proVersion.versionName, userName=user.userName,
                                          emailAddr=user.emailAddr)
         ac.delete()
-    return redirect('/index')
+        return HttpResponse(json.dumps({"status": 0, "msg": "取消订阅成功！"}), content_type="application/json")
+    return HttpResponse(json.dumps({"status": 1, "msg": "操作失败！请重新登录进行尝试！"}), content_type="application/json")
 
 
 # 更新用户信息页
@@ -202,15 +204,9 @@ def userSetting(request):
     # 获取用户
     user = UserClass.objects.get(userName=request.session["username"])
 
-    # 查看是否有异常信息，返回到更新页
-    err = ""
-    if "updateErr" in request.session:
-        err = request.session["updateErr"]
-        del request.session["updateErr"]
+    # 到更新页
     return render(request, 'setting.html',
-                  {'user': user,
-                   'err': err,
-                   'productLine': request.session["productLine"]})
+                  {'user': user})
 
 
 # 更新用户信息操作
@@ -219,23 +215,21 @@ def userUpdate(request):
     if "username" not in request.session or "userAuth" not in request.session or "productLine" not in request.session:
         return redirect('/login')
 
-    # 未填必填项
+    # 非正常请求，跳转到登录页面
     if "username" not in request.POST and "password" not in request.POST and "password2" not in request.POST and "email" not in request.POST and "productline" not in request.POST:
-        request.session["updateErr"] = '请输入用户名、密码、邮箱、产品线'
-        return redirect('/userSetting')
-
-    # 判断两次输入密码是否一致
-    if request.POST['password'] != request.POST['password2']:
-        request.session["updateErr"] = '两次输入密码不一致'
-        return redirect('/userSetting')
-
-    # 查询用户是否存在
-    userList = UserClass.objects.filter(id=int(request.POST["userID"]))
-    if len(userList) != 1:
-        return redirect('/logout')
+        return redirect('/login')
 
     # 首先获取到用户原信息
     user = UserClass.objects.get(id=int(request.POST["userID"]))
+
+    # 对比原用户名和新用户名
+    if user.userName != request.POST["username"]:
+        if len(UserClass.objects.filter(userName=request.POST["username"])) > 0:
+            return HttpResponse(json.dumps({"status": 1, "msg": "用户名已存在！"}), content_type="application/json")
+
+    if user.emailAddr != request.POST["email"]:
+        if len(UserClass.objects.filter(emailAddr=request.POST["email"])) > 0:
+            return HttpResponse(json.dumps({"status": 1, "msg": "邮箱已存在！"}), content_type="application/json")
 
     # 更新订阅表
     acList = AbonnementClass.objects.filter(userName=user.userName, emailAddr=user.emailAddr)
@@ -262,7 +256,7 @@ def userUpdate(request):
     # 更新session
     request.session["username"] = request.POST["username"]
     request.session["productLine"] = request.POST["productline"]
-    return redirect("/index")
+    return HttpResponse(json.dumps({"status": 0, "msg": "修改信息成功！即将跳转到首页~"}), content_type="application/json")
 
 
 # 退出登录
@@ -274,30 +268,31 @@ def logout(request):
 
 # 查看订阅人(api接口，不判断是否登录)
 def viewVersionUsers(request):
-    resp = {}
+    resp = {"status": 1, "msg": {}}
 
     # 单个版本
     if "versionName" in request.GET:
         versionName = request.GET["versionName"]
         if versionName != "":
-            resp = {versionName: []}
+            resp["msg"] = {versionName: []}
             acList = AbonnementClass.objects.filter(versionName=versionName)
             for ac in acList:
-                resp[versionName].append(ac.userName)
+                resp["status"] = 0
+                resp["msg"][versionName].append(ac.emailAddr)
 
     # 所有版本
     else:
         acList = AbonnementClass.objects.all()
         if len(acList) > 0:
             for ac in acList:
-                if resp.has_key(ac.versionName) is False:
-                    resp[ac.versionName] = [ac.emailAddr]
+                if resp["msg"].has_key(ac.versionName) is False:
+                    resp["msg"][ac.versionName] = [ac.emailAddr]
                 else:
-                    resp[ac.versionName].append(ac.emailAddr)
+                    resp["msg"][ac.versionName].append(ac.emailAddr)
         else:
             versionList = ProductVersion.objects.all()
             for vl in versionList:
-                if resp.has_key(vl.versionName) is False:
-                    resp[vl.versionName] = []
+                if resp["msg"].has_key(vl.versionName) is False:
+                    resp["msg"][vl.versionName] = []
 
     return HttpResponse(json.dumps(resp), content_type="application/json")
